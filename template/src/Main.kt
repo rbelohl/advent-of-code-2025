88839590a11d44a41fc8aefe4501121732cda9a5
Radek Bělohlávek <radek.belohlavek@email.cz>
2025-12-02 14:36:59 +0100

    template

---
+++ template/src/Main.kt
@@ -0,0 +1,13 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+    val filename = args[0]
+
+    val lines = File(filename).readLines()
+
+    val part1 = 0
+    val part2 = 0
+
+    println(part1)
+    println(part2)
+}
