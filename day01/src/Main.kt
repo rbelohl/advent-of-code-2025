a8ea53fd344842cfd08e2cafad90b59df9c5d8cd
Radek Bělohlávek <radek.belohlavek@email.cz>
2025-12-02 18:09:31 +0100

    day 01 small cleanup

--- day01/src/Main.kt
+++ day01/src/Main.kt
@@ -13,11 +13,11 @@ fun main(args: Array<String>) {
 
     val part1 = rotations.runningFold(50) { acc, rotation -> (acc + rotation).mod(100) }.count{ it == 0 }
 
-    val part2 = rotations.runningFold(Pair(0,50)) { acc, rotation ->
-        val zeroCount = ((acc.second + rotation) / 100).absoluteValue + if (acc.second != 0 && (acc.second + rotation) <= 0) 1 else 0
-        val newPosition = (acc.second + rotation).mod(100)
-        Pair(zeroCount, newPosition)
-    }.sumOf { it.first }
+    val part2 = rotations.fold(Pair(0,50)) { acc, rotation ->
+        val newPosition = (acc.second + rotation)
+        val zeroCount = (newPosition / 100).absoluteValue + if (acc.second != 0 && newPosition <= 0) 1 else 0
+        Pair(acc.first + zeroCount, newPosition.mod(100))
+    }.first
 
     println(part1)
     println(part2)
