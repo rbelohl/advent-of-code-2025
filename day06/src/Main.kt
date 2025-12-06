d570a5188f3e29934cddfc5f584b491aacbe4f9d
Radek Bělohlávek <radek.belohlavek@email.cz>
2025-12-06 14:30:34 +0100

    day 06 part 2

--- day06/src/Main.kt
+++ day06/src/Main.kt
@@ -10,25 +10,61 @@ fun <T> List<List<T>>.transpose() : List<List<T>> {
     }
 }
 
+fun <T> List<T>.split(predicate: (T) -> Boolean) : List<List<T>> {
+    val result = mutableListOf<List<T>>()
+    val tmp = mutableListOf<T>()
+
+    this.forEach {
+        if (predicate(it)) {
+            result.add(tmp.toList())
+            tmp.clear()
+        } else {
+            tmp.add(it)
+        }
+    }
+    result.add(tmp.toList())
+
+    return result.toList()
+}
+
+fun getOperation(str: String) : (Long, Long) -> Long {
+    return when (str) {
+        "+" -> { a, b -> a + b }
+        "*" -> { a, b -> a * b }
+        else -> throw Exception()
+    }
+}
+
 fun main(args: Array<String>) {
     val filename = args[0]
 
     val lines = File(filename).readLines()
 
-    val input = lines.map { line ->
+    val inputPart1 = lines.map { line ->
         line.split(" " ).filter{ it.isNotBlank() }
     }.transpose()
 
-    val part1 = input.sumOf {
-        val operation = when (it.last()) {
-            "+" -> { a: Long, b: Long -> a + b }
-            "*" -> { a: Long, b: Long -> a * b }
-            else -> throw Exception()
-        }
+    val part1 = inputPart1.sumOf {
+        val operation = getOperation(it.last())
         it.dropLast(1).map(String::toLong).reduce(operation)
     }
 
-    val part2 = 0
+    val inputPart2 = lines.dropLast(1)
+        .map { it.toList() }
+        .transpose()
+        .map { it.joinToString("") }
+    val operations = lines.last()
+        .split(" ")
+        .filter { it.isNotBlank() }
+        .map(::getOperation)
+
+    val part2 = inputPart2.split { it.isBlank() }
+        .zip(operations)
+        .sumOf { (list, operation) ->
+            list.map(String::trim)
+                .map(String::toLong)
+                .reduce(operation)
+        }
 
     println(part1)
     println(part2)
